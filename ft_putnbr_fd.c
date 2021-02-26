dad42a6aebe4937e8238c5841b1e08bbbc828343
Ana luisa Sobreiro goncalves <asobreir@c1r9s4.42lisboa.com>
2021-02-26 18:06:53 +0000

    fixed putnbr and strnstr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: asobreir <asobreir@42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 14:51:13 by asobreir          #+#    #+#             */
-/*   Updated: 2021/02/16 14:52:59 by asobreir         ###   ########.fr       */
+/*   Updated: 2021/02/26 16:35:06 by asobreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == (-2147483647 - 1))
+	{
 		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
+		return ;
+	}
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		n = -n;
 	}
-	else if (n >= 10)
+	if (n >= 10)
 	{
 		ft_putnbr_fd((n / 10), fd);
 		ft_putchar_fd(n % 10 + '0', fd);
